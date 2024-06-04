fb194789de20514c77816192a65b8806ebaa2b08
Tumaini <tumainikebaya.234@gmail.com>
2024-06-04 03:42:05 +0300

    Adds Keypad and EyesOnMe

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleInput(){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleInput}/>
+        </div>
     )
 }
 
